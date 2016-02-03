2b444ff857442f0cc96cfc9d837afebcb6af3fee
fdiazcaballero <fdiazcaballero@gmail.com>
2016-02-03 16:27:10 +0100

    added DoctrineEnumBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new AppBundle\AppBundle(),
             new FOS\UserBundle\FOSUserBundle(),
             new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
+            new Fresh\DoctrineEnumBundle\FreshDoctrineEnumBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
